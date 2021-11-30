e6b22889fd2bf99df7abb8e76dbe7768f1b75588
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-11-30 20:32:30 -0300

    Finish.

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,4 +1,7 @@
 export function averageRating(ratings: number[]): string {
+  if (ratings.length === 0) {
+    return 'Sem avaliação';
+  }
   const sum = ratings.reduce((a, b) => a + b, 0);
   return (sum / ratings.length).toFixed(1);
 }
